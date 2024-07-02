8f386d21ea2d46c164acc227997fe2a6bacf7368
silver-coding-blockchain <emilyjohn.strongman@gmail.com>
2024-07-01 21:37:34 -0500

    update frontend

--- frontend/apps/dashboard/components/allow-list/modals/add-rule-wizard/components/button-radio-box.component.tsx
+++ frontend/apps/dashboard/components/allow-list/modals/add-rule-wizard/components/button-radio-box.component.tsx
@@ -3,9 +3,9 @@ import { chakra } from '@chakra-ui/react';
 export const ButtonRadioBox = chakra('div', {
   baseStyle: {
     borderWidth: '3px',
-    px: '4',
+   /* px: '4',
     pt: '4',
-    pb: '4',
+    pb: '4',*/
     borderRadius: 'md',
     cursor: 'pointer',
     transition: 'all 0.2s',
